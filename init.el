f65842556ad26612b42221a88f8ad8b790bd1851
Torsten Bögershausen <tboegi@web.de>
2016-02-12 15:02:40 +0100

    Add changes for Linux

--- init.el
+++ init.el
@@ -130,3 +130,42 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+########################################################
+# https://www.kernel.org/doc/Documentation/CodingStyle #
+########################################################
+
+
+
+(defun c-lineup-arglist-tabs-only (ignored)
+  "Line up argument lists by tabs, not spaces"
+  (let* ((anchor (c-langelem-pos c-syntactic-element))
+         (column (c-langelem-2nd-pos c-syntactic-element))
+         (offset (- (1+ column) anchor))
+         (steps (floor offset c-basic-offset)))
+    (* (max steps 1)
+       c-basic-offset)))
+
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            ;; Add kernel style
+            (c-add-style
+             "linux-tabs-only"
+             '("linux" (c-offsets-alist
+                        (arglist-cont-nonempty
+                         c-lineup-gcc-asm-reg
+                         c-lineup-arglist-tabs-only))))))
+
+(add-hook 'c-mode-hook
+          (lambda ()
+            (let ((filename (buffer-file-name)))
+              ;; Enable kernel mode for the appropriate files
+              (when (and filename
+                         (string-match (expand-file-name "~/src/linux-trees")
+                                       filename))
+                (setq indent-tabs-mode t)
+                (setq show-trailing-whitespace t)
+                (c-set-style "linux-tabs-only")))))
+
+#This will make emacs go better with the kernel coding style for C
+#files below ~/src/linux-trees.
+
