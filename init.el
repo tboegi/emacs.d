a7f5d02d96a4fbf5f18fb4d31d0a355bc3321504
Torsten Bögershausen <tboegi@web.de>
2016-02-13 11:51:24 +0100

    Remove commented out code

--- init.el
+++ init.el
@@ -21,25 +21,6 @@
                 (setq show-trailing-whitespace t)
                 (c-set-style "linux-tabs-only")))))
 
-;;(add-hook 'sh-mode-hook
-;;          (lambda ()
-;;            (let ((filename (buffer-file-name)))
-;;              ;; Enable kernel mode for the appropriate files
-;;              (when (and filename
-;;                         (string-match (expand-file-name "~/projects/git")
-;;                                       filename))
-;;                (setq indent-tabs-mode t)
-;;                (setq tab-width 2)
-;;		(setq sh-indentation 2)
-;;		(setq sh-basic-offset 2)
-;;		(setq sh-indent-for-then 2)
-;;		(setq sh-indent-for-case-alt 2)
-;;		(sh-handle-prev-case-alt-end 2)
-;;		(setq sh-indent-for-case-label 2)
-;;
-;;                (setq show-trailing-whitespace t)
-;;                (c-set-style "linux-tabs-only")))))
-
 (add-hook 'sh-mode-hook
           '(lambda ()
              (setq tab-width 8)
@@ -54,10 +35,4 @@
              (setq fill-column 70)
              ))
 
-
-;;sh-indent-for-case-label 0
-;;        sh-indent-for-case-alt '+
-
-;;This will make emacs go better with the kernel coding style for C
-;;files below ~/src/linux-trees.
 
