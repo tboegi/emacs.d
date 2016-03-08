29c37090b476e13d4619cf0c9df3db8e236bc306
Torsten Bögershausen <tboegi@web.de>
2016-03-08 07:42:12 +0100

    Add hack for Mac. sh something

--- init.el
+++ init.el
@@ -1,6 +1,17 @@
+;; Default, cloumn and line number, no TAB
 (custom-set-variables
  '(column-number-mode t)
- '(line-number-mode t))
+ '(line-number-mode t)
+ '(setq tab-width 2)
+ '(indent-tabs-mode nil)
+)
+
+;; Hack for Mac
+(when (eq system-type 'darwin) ;; mac specific settings
+;;  ;;(setq mac-option-modifier 'alt)
+;;  ;;(setq mac-command-modifier 'meta)
+  (global-set-key [delete] 'delete-char) ;; sets fn-delete to be right-delete
+)
 
 
 (add-hook 'c-mode-common-hook
@@ -46,7 +57,19 @@
              (sh-handle-prev-case-alt-end 0)
              (c-set-style "linux-tabs-only")
              (setq fill-column 70)
-             )
+	     (let ((filename (buffer-file-name)))
+	       ;; Enable kernel mode for the appropriate files
+	       (when (and filename
+			  (string-match (expand-file-name "~/projects/git")
+					filename))
+		 (setq indent-tabs-mode t)
+		 (setq tab-width 8)
+		 (setq show-trailing-whitespace t)
+		 (c-set-style "linux-tabs-only")
+		 )
+	       )
+	     )
 	  )
+
 
 
