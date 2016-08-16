6d901c7912c461551e563f3276b3871fc1f2103c
Torsten Bögershausen <tboegi@web.de>
2016-08-16 06:00:38 +0200

    Rmove the tool bar; smaller font

--- init.el
+++ init.el
@@ -4,6 +4,7 @@
  '(line-number-mode t)
  '(setq tab-width 2)
  '(indent-tabs-mode nil)
+ '(tool-bar-mode nil)
 )
 
 ;; Hack for Mac
@@ -71,5 +72,11 @@
 	     )
 	  )
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 90 :width normal)))))
 
 
